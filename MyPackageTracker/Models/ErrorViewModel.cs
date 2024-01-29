3dce517d259cca78fbee1bd5c7e7a1d13993366b
Chukwuemeka <sylvesterchukso@gmail.com>
2024-01-29 21:09:50 +0000

    The initial commit

---
+++ MyPackageTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPackageTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
